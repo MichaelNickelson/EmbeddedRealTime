9d512fb7eb5265e765279c7df7bcc197d1f27a27
Michael Nickelson <michael.nickelson@gmail.com>
2014-01-26 22:55:04 -0500

    Add comments to packet parser.

--- Program1/DateDemo/App/PktParser.c
+++ Program1/DateDemo/App/PktParser.c
@@ -1,10 +1,13 @@
 /*---------------------------- FILE: PktParser.c ----------------------------*/
 
+/* Include Micrium and STM headers. */
 #include "includes.h"
 
+/* Include own header and header for error handling */
 #include "PktParser.h"
 #include "Errors.h"
 
+/* Define parameters of packet */
 #define HeaderLength 4
 #define ShortestPacket 8
 
@@ -18,26 +21,24 @@ typedef struct
   CPU_INT08U data[1];
 } PktBfr;
 
+/* Function for packet handling */
 void ParsePkt(void *pktBfr)
 {
-  CPU_INT08U preamble[HeaderLength-1] = {0x03, 0xEF, 0xAF};
-  
+  /* Initialize variables needed for packet parsing */
+  CPU_INT08U preamble[HeaderLength-1] = {0x03, 0xEF, 0xAF}; 
   CPU_INT08U checkSum = 0;
-  
   ParserState   parseState = P1;
   CPU_INT16S    c;
   CPU_INT08U    i;
-  
   PktBfr *payloadBfr = pktBfr;
+  
   for(;;){
     c = GetByte();
-    
-    checkSum ^= c;
-//    BSP_Ser_Printf("%x", c);
-//    BSP_Ser_Printf(" ");
+    checkSum ^= c; // Maintain running checksum as packets are received
     
     switch (parseState){
       case P1:
+        // Start looking for a packet, if anything is wrong, raise an error
         if (c == preamble[0]){
           parseState = P2;
         }else{
@@ -55,6 +56,7 @@ void ParsePkt(void *pktBfr)
         break;
       case P3:
         if (c == preamble[2]){
+          // Found a full preamble, read in the packet length
           parseState = L;
         }else{
           DispErr(ERR_PRE3);
@@ -62,16 +64,18 @@ void ParsePkt(void *pktBfr)
         }
         break;
       case L:
+        // Read in packet length, subtract header length to get payload length
         payloadBfr->payloadLen = c - HeaderLength;
-        if(c<ShortestPacket){
+        if(c<ShortestPacket){ // Raise an error if the packet is too short
           DispErr(ERR_LEN);
           parseState = ER;
         }else{
-        i = 0;
+        i = 0; // Start reading in data, starting with byte 0
         parseState = R;
         }
         break;
       case R:
+        // Read in data
         payloadBfr->data[i++] = c;
         if (i>= payloadBfr->payloadLen){
           if(checkSum){
